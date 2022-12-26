725188a29707a8f2503814cb89eb91ba885d2cf7
bharathikannansambasivam <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-12-26 20:31:31 +0530

    Add files via upload

---
+++ recursion1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+    int n;
+      printf("enter the value of n");
+    scanf("%d", &n);
+
+    printf("%d", sum(n));
+    return 0;
+}
+
+int sum(int n)
+{
+
+    if (n == 1)
+        return n;
+    return n + sum(n - 1);
+}
