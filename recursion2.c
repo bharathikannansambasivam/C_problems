e64ae768d4540428368564278c6dd17898d4d249
bharathikannansambasivam <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-12-27 21:09:34 +0530

    Add files via upload

---
+++ recursion2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <math.h>
+int n, r, sum = 0, power = 0;
+int show(int n)
+{
+
+    if (n <= 0)
+        printf("%d", sum);
+    else
+    {
+        r = n % 10;
+        sum = sum + r * pow(2, power);
+        // n = n / 10;
+        power = power + 1;
+
+        show(n / 10);
+    }
+    return 0;
+}
+void main()
+{
+
+    printf("enter binary number");
+    scanf("%d", &n);
+
+    show(n);
+}
