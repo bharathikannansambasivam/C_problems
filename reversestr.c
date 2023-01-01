c25e3cc5e9d1ce13a49aaca220fd24922b584e8d
bharathikannansambasivam <91874993+bharathikannansambasivam@users.noreply.github.com>
2023-01-01 07:18:52 +0530

    Add files via upload

---
+++ reversestr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100], temp;
+    int len, i;
+    printf("enter the string \t");
+    scanf("%s", str);
+    len = strlen(str);
+ 
+
+    for (i = 0; i < len / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[len -i-1];
+        str[len -i- 1] = temp;
+    
+    }
+    printf("After reversing the string :%s", str);
+}
